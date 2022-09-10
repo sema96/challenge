8da50dd34687e9edb97bd43e80b999bf22465288
sema96 <semanur@gmx.net>
2022-09-10 19:09:02 +0200

    reading the football.csv file and writing the data into an ArrayList

--- src/main/java/getdata/ReadCSV.java
+++ src/main/java/getdata/ReadCSV.java
@@ -3,17 +3,19 @@ import java.io.*;
 import java.util.ArrayList;
 
 import de.exxcellent.challenge.models.WeatherData;
+import de.exxcellent.challenge.models.FootballData;
 
-//class that reads the csv file
 public class ReadCSV
 {  
+	//initialization is global, because it is needed for both methods to read and split the 
+	//data from the csv file
+	
+	private String line = "";  
+	private String splitBy = ","; 
 	
 //code for reading the relevant data from the weather.csv file, returns an ArrayList
 public ArrayList <WeatherData> readWeatherData() 
 {  
-	 String line = "";  
-	 String splitBy = ","; 
-	
 
 	//using the WeatherData model, for storing the data
 	ArrayList<WeatherData>  weatherData=new ArrayList<>();
@@ -32,7 +34,7 @@ public ArrayList <WeatherData> readWeatherData()
 	
 	//column, which contains the day
 	String day=data[0];
-	//column, which contains the maximum temperature, parsing, because it is necessary
+	//column, which contains the maximum temperature
 	double mxt=Double.parseDouble(data[1]);
 	//column, which contains the minimum temperature
 	double mnt=Double.parseDouble(data[2]);
@@ -51,4 +53,44 @@ public ArrayList <WeatherData> readWeatherData()
 	//ArrayList with the WeatherData is returned
 	return weatherData;
 }
+
+//code for reading the relevant data from the football.csv file, returns an ArrayList
+public ArrayList <FootballData> readFootballData() 
+{  
+	
+	//using the FootballData model, for storing the data
+	ArrayList<FootballData>  footballData=new ArrayList<>();
+	try   
+	{  
+	//parsing a CSV file into BufferedReader class constructor  
+	BufferedReader br = new BufferedReader(new FileReader("src\\main\\resources\\de\\exxcellent\\challenge\\football.csv"));  
+	
+	//skipping first csv line
+	br.readLine();
+	while ((line = br.readLine()) != null)   //returns a Boolean value  
+	{  
+		
+	String[] data = line.split(splitBy); 
+	
+	//column, which contains the name of the team
+	String teamName=data[0];
+	//column, which contains the amount of goals the team made
+	int goals=Integer.parseInt(data[5]);
+	//column, which contains the amount of goals, which are actually allowed
+	int goalsAllowed=Integer.parseInt(data[6]);
+	
+	//the data is stored in the ArrayList
+	
+	footballData.add(new FootballData(teamName,goals, goalsAllowed));
+	} 
+	
+	}   
+	catch (IOException e)   
+	{  
+	e.printStackTrace();  
+	}  
+	
+	//ArrayList with the FootballData is returned
+	return footballData;
 }
+}  
