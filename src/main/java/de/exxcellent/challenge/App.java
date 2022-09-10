34455335f1ba848dc84d1b9b070dd86faf6fa54f
sema96 <semanur@gmx.net>
2022-09-10 19:12:18 +0200

    inserted the code, which gets the smallest distance of goals

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -23,7 +23,7 @@ public final class App {
         String dayWithSmallestTempSpread = getInformation.getDayWithSmallestSpread();       // Your day analysis function call …
         System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
 
-        String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
-        System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
-    }
+      //returns the team with the smallest distance
+        String teamWithSmallestGoalSpread = getInformation.getTeamNameWithSmallestDistance(); // Your goal analysis function call …
+        System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);}
 }
--- src/main/java/getdata/GetInformation.java
+++ src/main/java/getdata/GetInformation.java
@@ -4,15 +4,17 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 import de.exxcellent.challenge.models.WeatherData;
-
+import de.exxcellent.challenge.models.FootballData;
 
 //this class gives the actual information, which is asked in the task
 public class GetInformation {
 
+	//the initialization is global, because it is needed in both tasks
+	private ReadCSV getData = new ReadCSV();
+		 
 	//this method searches the day of the minimum temperature spread and returns it 
 	public String getDayWithSmallestSpread () {
-		
-			ReadCSV getData = new ReadCSV();
+
 			//needed to find the minimum spread
 		 ArrayList <Double> differences=new ArrayList<>();
 		 
@@ -36,10 +38,53 @@ public class GetInformation {
 				 dayWithSmallestTempSpread= element.getDay();
 			 }
 			}
+		 
 		 //returns the day with the smallest spread
 		return dayWithSmallestTempSpread;
 				
 	}
 	
+	//this method searches the name of the team with the smallest goal distance 
+	public String getTeamNameWithSmallestDistance () {
+
+		//needed for finding the minimum distance
+		 ArrayList <Integer> distance=new ArrayList<>();
+		 
+		 //teamname, needed for return statement
+		 String teamName ="";
+		 
+		//for all football data elements, the distances are added in the ArrayList
+		for(FootballData element :  getData.readFootballData())
+		{
+			//assigning to a variable, needed to consider negative values
+			int getDistance=element.getDistance();
+			
+	//negative value is transformed into positive value, since the smallest distance is needed
+			if(getDistance<0) {
+				getDistance=-getDistance;
+			}
+			
+			//all distances are added into the array, for finding the teamname
+			distance.add(getDistance);	
+		}
+		
+		//Function, which finds the smallest distance
+		 int smallestDistance = Collections.min(distance);
+
+//searches the name of the team with the smallest distance spread, by using the variable
+//smallest Distance
+		
+		 for(FootballData element :  getData.readFootballData())
+			{
+	//positive, as well as negative values are considered here
+			 
+			 if(element.getDistance()==smallestDistance || element.getDistance()==-smallestDistance ) {
+				 teamName= element.getTeamName();
+			 }
+			}
+		//returns the name of the team
+		return teamName;
+				
+	}
 }
 		
